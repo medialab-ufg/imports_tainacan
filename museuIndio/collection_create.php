26bae23356ca6633f525de6800fc272de3e35b83
LuisMDlab <luisfelipeprf@gmail.com>
2018-06-05 21:39:31 -0300

    Update collection_create.php

--- museuIndio/collection_create.php
+++ museuIndio/collection_create.php
@@ -1,9 +1,6 @@
 <?php
 #Connecting with Wordpress:
 $_SERVER['SERVER_PROTOCOL'] = "HTTP/1.1";
-<?php
-#Connecting with Wordpress:
-$_SERVER['SERVER_PROTOCOL'] = "HTTP/1.1";
 $_SERVER['REQUEST_METHOD'] = "GET";
         
 define( 'WP_USE_THEMES', false );
@@ -21,19 +18,21 @@ foreach ($collections as $col) {
 
 #Create Collection and Metadata:
 $fieldsRepo = \Tainacan\Repositories\Fields::get_instance();
+$taxonomyRepo = \Tainacan\Repositories\Taxonomies::get_instance();
+
 $collection = new \Tainacan\Entities\Collection();
 $collection->set_name('Museu do Índio');
 $collection->set_status('publish');
 $collection->set_description('Coleção com informações sobre os itens museológicos do Museu do Índio.');
 
-$taxonomyRepo = \Tainacan\Repositories\Taxonomies::get_instance();
+#Creating instance of taxonomy
+
+
 
 $cont = 0;
 if ($collection->validate()) {
-
 	$insertedCollection = $collectionsRepo->insert($collection);
-	
-	
+
 	if (($handle = fopen("metadadosMIndio(teste).csv", "r")) == TRUE) {
 		
 		$collection_core_fields = $fieldsRepo->get_core_fields($insertedCollection);
@@ -59,24 +58,33 @@ if ($collection->validate()) {
 					}
 				} else {
 					if (trim($data[2]) == 'Tainacan\Field_Types\Category'){
+						
 						$taxonomy = new \Tainacan\Entities\Taxonomy;
 						$taxonomy->set_name(trim($data[0]));
-						$taxonomy->set_description("Taxonomia para o metadado ", trim($data[0]));
-						$taxonomy->set_allow_insert(True);
+						$taxonomy->set_description("Taxonomia para o metadado ". trim($data[0]));
+						$taxonomy->set_allow_insert('yes');
+						$taxonomy->set_status('publish');
+						
 						if ($taxonomy->validate()) {
-							$taxonomy = $taxonomyRepo->insert($taxonomy);
-							$taxonomy->set_status('publish');
+							$insertedTaxonomy = $taxonomyRepo->insert($taxonomy);
 							echo 'Taxonomy created with ID -  ' . $taxonomy->get_id(), "\n";
 							
-							} else {
-								$errors = $taxonomy->get_errors();
-							}
+						} else {
+								$error = $taxonomy->get_errors();
+								var_dump($error);
+						}
+						
+						$field_type_options = ['taxonomy_id' => $insertedTaxonomy->get_id(), 'input_type' => 'tainacan-category-tag-input', 'allow_new_terms' => 'yes'];
+						
 						$metadado = new \Tainacan\Entities\Field();
 						$metadado->set_collection($insertedCollection);
 						$metadado->set_name(trim($data[0]));
 						$metadado->set_description($data[1]);
-						$metadado->set_field_type_options(['taxonomy_id'=> $taxonomy->get_id()]);
-						$metadado->set_field_type(trim($data[2]));				
+						$metadado->set_field_type(trim($data[2]));
+						$metadado->set_multiple('yes');
+						$metadado->set_status('publish');
+						$metadado->set_field_type_options($field_type_options);
+
 						
 					}
 					else{
@@ -98,9 +106,8 @@ if ($collection->validate()) {
 			}
 		$cont+=1;
 		}
-	}
-
 	fclose($handle);
+	}
 	
 	if ($insertedCollection->validate()) {
 		$insertedCollection = $collectionsRepo->insert($insertedCollection);
