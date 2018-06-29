8243fe1b6dd74330f84641894d75c4a2adf100fe
luisfeliperd <luisfelipeprf@gmail.com>
2018-06-29 11:27:31 -0300

    Update item_update.php

--- museuIndio/item_update.php
+++ museuIndio/item_update.php
@@ -6,6 +6,8 @@ $_SERVER['REQUEST_METHOD'] = "GET";
         
 define( 'WP_USE_THEMES', false );
 define( 'SHORTINIT', false );
+
+#ALTERAR O PATH ONDE O wp-blog-header.php SE ENCONTRA
 require( '/var/www/html/wp-blog-header.php' );
 
 $collectionsRepo = \Tainacan\Repositories\Collections::get_instance();
@@ -13,9 +15,11 @@ $fieldsRepo = \Tainacan\Repositories\Fields::get_instance();
 $itemsRepo = \Tainacan\Repositories\Items::get_instance();
 $itemMedia = \Tainacan\Media::get_instance();
 
+#NOME DO METADADO DO ITEM
 $fieldDocumento = $fieldsRepo->fetch(['name'=>'V018 – Nome do Objeto'], 'OBJECT');
 $fieldDocumento = $fieldDocumento[0];
 
+#BUSCA POR ITEM
 $items = $itemsRepo->fetch(['title' => 'Sugerir Excluir', 'posts_per_page' => -1], $fieldDocumento->get_collection(), 'OBJECT');
 
 foreach($items as $item){
