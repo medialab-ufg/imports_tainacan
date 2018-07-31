1f3f1dab9c5bdfc6edee2c523a3cdd01fffe719c
luisfeliperd <luisfelipeprf@gmail.com>
2018-07-31 17:22:09 -0300

    Update collection_create.php

--- museuIndio/collection_create.php
+++ museuIndio/collection_create.php
@@ -6,60 +6,49 @@ $_SERVER['REQUEST_METHOD'] = "GET";
 define( 'WP_USE_THEMES', false );
 define( 'SHORTINIT', false );
 
-#ALTERAR O PATH ONDE O wp-blog-header.php SE ENCONTRA
-require( '/var/www/html/wp-blog-header.php' );
+#Path of wp
+require( '/home/l3p/apache_sites/museudarepublica.medialab.ufg.br/web/wp-blog-header.php' );
 
-#Delete Existing Collections:
 $collectionsRepo = \Tainacan\Repositories\Collections::get_instance();
-
-#O BLOCO ASSEGUIR DELETA COLEÇÕES JÁ EXISTENTES - PARA ATIVÁ-LO RETIRE OS COMENTÁRIOS
-/*
-$collections = $collectionsRepo->fetch([], 'OBJECT');
-foreach ($collections as $col) {
-	echo "Deleting collection ", $col->get_id(), "\n";
-	$collectionsRepo->delete([$col->get_id(), true]);
-}
-*/
-
-#Create Collection and Metadata:
 $metadataRepo = \Tainacan\Repositories\Metadata::get_instance();
 $taxonomyRepo = \Tainacan\Repositories\Taxonomies::get_instance();
 
-#INSERIR INFORMAÇÕES SOBRE A COLEÇÃO
+
+#Create Collection and Metadata:
 $collection = new \Tainacan\Entities\Collection();
-$collection->set_name('Museu do Índio');
+$collection->set_name('Museu do Índio - Julho/18');
 $collection->set_status('publish');
-$collection->set_description('Coleção com informações sobre os itens museológicos do Museu do Índio.');
+$collection->set_description('Coleção com informações sobre os objetos do Museu do Índio.');
 
 $cont = 0;
 if ($collection->validate()) {
 	$insertedCollection = $collectionsRepo->insert($collection);
 
-	# MODIFICAR O NOME DO ARQUIVO E SE HOUVER, O CAMINHO PARA O ARQUIVO CSV
-	if (($handle = fopen("metadadosMIndio(teste).csv", "r")) == TRUE) {
+	if (($handle = fopen("metadados_mindio.csv", "r")) == TRUE) {
 		
-		$collection_core_metadata = $metdataRepo->get_core_metadata($insertedCollection);
+		$collection_core_metadata = $metadataRepo->get_core_metadata($insertedCollection);
 		
 		while (($data = fgetcsv($handle, 0, ",")) == TRUE){
 			if ($cont == 0){
 				echo "Pulando linha das colunas", "\n";
 			} else{
-				if (trim($data[3])){
-					foreach ($collection_core_metadata as $coremetadata) {
-						if($coremetadata->get_name() == $data[3]){
-							$coremetadata->set_name($data[0]);
-							$coremetadata->set_description($data[1]);
-							echo "Atualizando Core Metadata: ", $data[0], "\n";
+				if (trim($data[3]) == 'Description' OR trim($data[3]) == 'Title'){
+					foreach ($collection_core_metadata as $coreMetadata) {
+						if($coreMetadata->get_name() == $data[3]){
+							$coreMetadata->set_name($data[0]);
+							$coreMetadata->set_description($data[1]);
+							echo "Atualizando Core Field: ", $data[0], "\n";
 						
-							if ($coremetadata->validate()){
-								$insertedMetadata = $metadataRepo->insert($coremetadata);
+							if ($coreMetadata->validate()){
+								$insertedMetadata = $metadataRepo->insert($coreMetadata);
 							} else {
-								$erro = $coremetadata->get_errors();
+								$erro = $coreMetadata->get_errors();
 								var_dump($erro);
 							}
 						}
 					}
 				} else {
+					
 					if (trim($data[2]) == 'Tainacan\Metadata_Types\Taxonomy'){
 						
 						$taxonomy = new \Tainacan\Entities\Taxonomy;
@@ -77,7 +66,7 @@ if ($collection->validate()) {
 								var_dump($error);
 						}
 						
-						$metadata_type_options = ['taxonomy_id' => $insertedTaxonomy->get_id(), 'input_type' => 'tainacan-category-tag-input', 'allow_new_terms' => 'yes'];
+						$metadata_type_options = ['taxonomy_id' => $insertedTaxonomy->get_id(), 'input_type' => 'tainacan-taxonomy-tag-input', 'allow_new_terms' => 'yes', 'multiple'=>'yes'];
 						
 						$metadado = new \Tainacan\Entities\Metadatum();
 						$metadado->set_collection($insertedCollection);
@@ -86,16 +75,18 @@ if ($collection->validate()) {
 						$metadado->set_metadata_type(trim($data[2]));
 						$metadado->set_multiple('yes');
 						$metadado->set_status('publish');
+						$metadado->set_display('no');
 						$metadado->set_metadata_type_options($metadata_type_options);
-
-						
-					} else{
+					
+					}else{
+					
 						$metadado = new \Tainacan\Entities\Metadatum();
 						$metadado->set_collection($insertedCollection);
 						$metadado->set_name(trim($data[0]));
 						$metadado->set_description($data[1]);
-						$metadado->set_metadatum_type(trim($data[2]));
+						$metadado->set_metadata_type(trim($data[2]));
 						$metadado->set_status('publish');
+						$metadado->set_display('no');
 					}
 				
 					if ($metadado->validate()){
@@ -104,11 +95,16 @@ if ($collection->validate()) {
 						$erro = $metadado->get_errors();
 						var_dump($erro);
 					}
+				
 				}
 			}
+			
 		$cont+=1;
+		
 		}
+		
 	fclose($handle);
+	
 	}
 	
 	if ($insertedCollection->validate()) {
@@ -120,6 +116,8 @@ if ($collection->validate()) {
 	
 }else {
 	$validationErrors = $collection->get_errors();
+	echo $validationErrors;
 	die;
 }
 ?>
+
