ed3f8568558658b8a45d62d88502c0b0b3a13794
luisfeliperd <luisfelipeprf@gmail.com>
2018-10-16 16:29:22 -0300

    Update insert_itens.php

--- museuIndio/insert_itens.php
+++ museuIndio/insert_itens.php
@@ -1,3 +1,4 @@
+
 <?php
 
 #Connecting to Wordpress
@@ -14,12 +15,12 @@ $metadataRepo = \Tainacan\Repositories\Metadata::get_instance();
 $itemsRepo = \Tainacan\Repositories\Items::get_instance();
 $itemMetadataRepo = \Tainacan\Repositories\Item_Metadata::get_instance();
 
-#Getting the Colletion -- # MODIFICAR O NOME DA COLEÇÃO PARA O DESEJADO
+#Getting the Colletion
 $collection = $collectionsRepo->fetch(['name'=>'Museu do Índio'], 'OBJECT');
 $collection = $collection[0];
 
-# MODIFICAR O NOME DO ARQUIVO E SE HOUVER, O CAMINHO PARA O ARQUIVO CSV
-$fh = fopen("baseMindio(teste).csv", "r") or die("ERROR OPENING DATA");
+
+$fh = fopen("itens.csv", "r") or die("ERROR OPENING DATA");
 
 while (($data = fgetcsv($fh, 0, ",")) == TRUE){
 	$linecount++;
@@ -29,45 +30,76 @@ fclose($fh);
 
 #Getting metadata title from csv array
 
-# MODIFICAR O NOME DO ARQUIVO E SE HOUVER, O CAMINHO PARA O ARQUIVO CSV
-if (($handle = fopen("baseMindio(teste).csv", "r")) == TRUE) {
+if (($handle = fopen("itens.csv", "r")) == TRUE) {
+	
 	$cont = 0;
 	
 	while (($data = fgetcsv($handle, 0, ",")) == TRUE){
 		
 		if($cont == 0){
+			
 			echo "Tratando primeira linha \n";
 			$headers = array_map('trim', $data);
+			
 		}else{
+			
 			$item = new \Tainacan\Entities\Item();
+			
 			$item->set_title($data[0]);
 			$item->set_status('publish');
 			$item->set_collection($collection);
 			
-			if ($item->validate())  {
-				$item = $itemsRepo->insert($item);
+			if ($item->validate()) {
 				
+				$item = $itemsRepo->insert($item);
 				for ($i = 0; $i <=sizeof($data); $i++) {
 					
-					$metadata = $metatadaRepo->fetch(['name' => $headers[$i]], 'OBJECT');
-					$metadata = $metadata[0];
+					$metadatum = $metadataRepo->fetch(['name' => $headers[$i]], 'OBJECT');
+					$metadatum = $metadatum[0];
 					
-					if ($metatada->get_metadata_type() == 'Tainacan\Metadata_Types\Category'){
-						$itemMetadata = new \Tainacan\Entities\Item_Metadata_Entity($item, $metadata);
-						$category_value = explode("||",$data[$i]);
-						$itemMetadata->set_value($category_value);
+					
+					if ($metadatum->get_metadata_type() == 'Tainacan\Metadata_Types\Taxonomy'){
+						
+						$itemMetadata = new \Tainacan\Entities\Item_Metadata_Entity($item, $metadatum);
+						$taxonomy_value = explode("||",$data[$i]);
+						$itemMetadata->set_value($taxonomy_value);
+						
+					} else if ($metadatum->get_metadata_type() == 'Tainacan\Metadata_Types\Relationship'){
+						
+						$itemMetadata = new \Tainacan\Entities\Item_Metadata_Entity($item, $metadatum);
+						$relationship_value = explode("||",$data[$i]);
+						$relationship_array = [];
+						
+						if (strpos($metadatum->get_name(), 'Pessoa')!== false){
+							foreach($relationship_value as $item_id){
+								echo $array_pessoas[$item_id]->get_id();
+								echo "\n";
+								$relationship_array [] = $array_pessoas[$item_id]->get_id();
+							}
+						}
+						else if (strpos($metadatum->get_name(), 'Entidade')!== false){
+							foreach($relationship_value as $item_id){
+								$relationship_array [] = $array_entidades[$item_id]->get_id();
+							}
+						}
+						
+						$itemMetadata->set_value($relationship_array);
+					
 					}else{
-						$itemMetadata = new \Tainacan\Entities\Item_Metadata_Entity($item, $metadata);
+						
+						$itemMetadata = new \Tainacan\Entities\Item_Metadata_Entity($item, $metadatum);
 						$itemMetadata->set_value($data[$i]);
 					}
 					
 					if ($itemMetadata->validate()) {
-						$itemMetadataRepo->insert($itemMetadata);			
-						} else {
-							echo 'Erro no metadado ', $metadata->get_name(), ' no item ', $data[0];
-							$erro = $itemMetadata->get_errors();
-							echo var_dump($erro);
-						}
+						
+						$itemMetadataRepo->insert($itemMetadata);
+						
+					}else {
+						echo 'Erro no metadado ', $metadatum->get_name(), ' no item ', $data[0];
+						$erro = $itemMetadata->get_errors();
+						echo var_dump($erro);
+					}
 				}
 				if ($item->validate()) {
 					$item = $itemsRepo->insert($item);
@@ -84,6 +116,10 @@ if (($handle = fopen("baseMindio(teste).csv", "r")) == TRUE) {
 				
 			}else {
 				echo 'Erro na linha ', $cont;
+				echo  "\n\n";
+				var_dump($item);
+				echo  "\n\n";
+				die;
 			}
 			
 		}
